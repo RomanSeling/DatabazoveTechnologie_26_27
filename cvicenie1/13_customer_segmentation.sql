b764479ac997d9166978872d369dbb5accf2b2d3
Roman <selingroman28@gmail.com>
2026-09-26 12:26:14 +0200

    Úloha 13 – Segmentácia zákazníkov

---
+++ cvicenie1/13_customer_segmentation.sql
@@ -0,0 +1,13 @@
+SELECT 
+    c.customer_name,
+    SUM(o.sales) AS total_sales,
+    AVG(o.discount) AS average_discount,
+    COUNT(o.order_id) AS total_orders,
+    CASE 
+        WHEN SUM(o.sales) > 2500 THEN 'VIP'
+        ELSE 'REGULAR'
+    END AS customer_type
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+GROUP BY c.customer_id, c.customer_name
+ORDER BY total_sales DESC;
